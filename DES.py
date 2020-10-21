4bba94529fb3d4ff7b058d671f8e2028e2628e1e
cam12-a <62108230+cam12-a@users.noreply.github.com>
2020-10-22 00:57:11 +0300

    Add files via upload

--- DES.py
+++ DES.py
@@ -19,7 +19,7 @@ class DES:
         if len(text64Block)%64!=0:
             for i in range(len(text64Block),64,1):
                 text64Block+='#'
-        return [self.str2hex(text64Block[0:32]),self.str2hex(text64Block[32:64])]
+        return [self.str2hex(text64Block[0:32]).hex(),self.str2hex(text64Block[32:64]).hex()]
 
     def AllBlock64To32Blocks(self):
         blockToReturn=[]
@@ -36,27 +36,48 @@ class DES:
         for i in range(256):
             j=randint(0,i)
             key+=alphabet[j%32]
-        key=[key[i:i+32] for i in range(0,len(key),32)]
-        KEY=[self.str2hex(key[i]).decode("UTF-8") for i in range(len(key))]
+        key=[self.str2hex(key[i:i+32]).hex() for i in range(0,len(key),32)]
+        KEY=[int(self.str2hex(key[i]),16) for i in range(len(key))]
         #print(KEY,len(KEY[0]))
-        return [KEY,key]
+        return key
 
     def functionValue(self,LeftSide,Key):
+        try:
+            LeftSide=int(LeftSide,16)
+        except TypeError:
+            LeftSide=LeftSide
+        Key=Key
+        print(LeftSide,Key)
         return LeftSide^Key
+        
+    def devideTo4bits(self,LeftSide):
+        LeftSideDevide=[]
+        for i in range(32,4):
+            LeftSideDevide.append(LeftSide[i:i+1])
+        return LeftSideDevide
+
 
     def roundText(self,LeftSide,key):
-        RightSide=""
-        for j in range(len(key)):
-            k=1
-            while k<=32:
-                for i in range(1,len(LeftSide),1):
+        RightSide=int(LeftSide,16)
+        print("LeftSideDDDDD",self.devideTo4bits(LeftSide))
+        for j in range(32):
+                for i in range(0,7,1):
                     RightSide=LeftSide
-                    LeftSide[i]=RightSide[i]^self.functionValue(LeftSide[i],key[j])
-                k+=1
+                    LeftSide=RightSide^self.functionValue(LeftSide,int(key[i],16))
         return [LeftSide,RightSide]
     
     def SBlock(self):
-        pass
+        S1=[12,4,6,2,10,5,11,9,14,8,13,7,0,3,15,1]
+        S2=[6,8,2,3,9,10,5,12,1,14,4,7,11,13,0,15]
+        S3=[11,3,5,8,2,15,10,13,14,1,7,4,12,9,6,0]
+        S4=[12,8,2,1,13,4,15,6,7,0,10,5,3,14,9,11]
+        S5=[7,15,5,10,8,1,6,13,0,9,3,14,11,4,2,12]
+        S6=[5,13,15,6,9,2,12,10,11,7,8,1,4,3,14,0]
+        S7=[8,14,2,5,6,9,1,12,15,4,11,0,13,10,3,7]
+        S8=[1,7,14,13,0,5,8,3,4,15,10,6,9,12,11,2]
+        S=[S1,S2,S3,S4,S5,S6,S7,S8]
+        return S
+        
 
     def str2hex(self,s):
         return binascii.hexlify(bytes(str.encode(s)))
@@ -65,19 +86,24 @@ class DES:
         return binascii.unhexlify(h)
     
 
-Des=DES("/home/kali/crypto/text.txt")
+Des=DES("/home/kali/crypto/variant.txt")
 textToBlock64=Des.openTextToBlock64()
-print(len(textToBlock64))
-print(len(Des.str2hex(textToBlock64[0]).decode("UTF-8")))
+#print(len(textToBlock64))
+#print(len(Des.str2hex(textToBlock64[0]).decode("UTF-8")))
 aa=Des.Block64ToTwoBlockOf32(textToBlock64[0])
 
 aa=Des.AllBlock64To32Blocks()
+print("KEY")
+#rint(Des.generateKey(),len(Des.generateKey()))
+print("TEXT")
+#print(aa)
 
-
-#print("dsf",len(aa))
-print(Des.hex2str(aa[0][0]).decode("UTF-8"))
-print(Des.hex2str(aa[0][1]).decode("UTF-8"))
-print(len(Des.hex2str(aa[0][1]).decode("UTF-8")))
-print(Des.generateKey()[0][1])
-print()
-print(Des.roundText(Des.hex2str(aa[0][0]).decode("UTF-8"),Des.generateKey()[0][0]))
+#print(Des.functionValue(aa[0][0],Des.generateKey()[0][1]))
+#print(int(aa[0][0],16)&int(aa[0][1],16))
+#print("dsdsf",type(int(aa[0][0],16)))
+#print(Des.hex2str(aa[0][0]).decode("UTF-8"))
+#print(Des.hex2str(aa[0][1]).decode("UTF-8"))
+#print(len(Des.hex2str(aa[0][1]).decode("UTF-8")))
+#print(type(Des.generateKey()[0][1]))
+print(Des.devideTo4bits(aa[0][0]))
+print(Des.roundText(aa[0][0],Des.generateKey()))
